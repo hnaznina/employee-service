3d10a3c8cedb3952776a4f14ed56d79685460136
z002brk <atik.rahman@target.com>
2018-12-05 20:56:29 -0600

    Swagger

---
+++ src/main/java/com/example/employeeservice/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.employeeservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+//Swagger provides a UI to test all endpoints or urls: http://localhost:8080/swagger-ui.html
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
+
